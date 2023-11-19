c1e5de5a5ccda86e6823b87c77577e772c9f66e8
Bohdan Melnychuk <62162-bobi-mel@users.noreply.drupalcode.org>
2023-11-19 11:24:50 +0200

    setUp() tearDown()

--- src/SalaryCalculator.php
+++ src/SalaryCalculator.php
@@ -2,8 +2,28 @@
 
 namespace App;
 
+use mysqli;
+
 class SalaryCalculator
 {
+    public function calculateSalary(int $accountId): float
+    {
+        $mysql = new mysqli('localhost', 'phpunit', 'phpunit', 'phpunit');
+        if ($mysql->error) {
+            echo $mysql->error . PHP_EOL;
+            exit(1);
+        }
+
+        $result = $mysql->query('SELECT * FROM account WHERE id = ' . $accountId);
+        $account = $result->fetch_assoc();
+        if (!$account) {
+            echo 'Account not found' . PHP_EOL;
+            exit(1);
+        }
+
+        return $this->calculate($account['salary']);
+    }
+
     public function calculate(float $salary): float
     {
         return round($salary * 0.9, 2);
--- tests/SalaryCalculatorTest.php
+++ tests/SalaryCalculatorTest.php
@@ -4,18 +4,49 @@ namespace Tests;
 
 
 use App\SalaryCalculator;
+use mysqli;
 use PHPUnit\Framework\TestCase;
 
 class SalaryCalculatorTest extends TestCase
 {
+    private $mysqli;
+
+    private SalaryCalculator $salaryCalculator;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->mysqli = new mysqli('localhost', 'phpunit', 'phpunit', 'phpunit');
+        if ($this->mysqli->error) {
+            echo $this->mysqli->error . PHP_EOL;
+            exit(1);
+        }
+
+        $this->salaryCalculator = new SalaryCalculator();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->mysqli->close();
+    }
+
+    public function testCalculateSalary()
+    {
+        $this->mysqli->query('INSERT INTO account SET email = "", name = "", salary = 11');
+        $accountId = $this->mysqli->insert_id;
+
+        $result = $this->salaryCalculator->calculateSalary($accountId);
+
+        self::assertEquals(9.9, $result);
+
+    }
 
     /**
      * @dataProvider dataProvider
      */
     public function testCalculate(float $salary, float $expected): void
     {
-        $salaryCalculator = new SalaryCalculator();
-        $result = $salaryCalculator->calculate($salary);
+        $result = $this->salaryCalculator->calculate($salary);
 
         self::assertEquals($expected, $result);
     }
